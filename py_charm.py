07ac47a9e2360a9c545037e1fb830adcc1f2f904
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2022-12-27 21:11:56 +0200

    Changed a function - def count

--- py_charm.py
+++ py_charm.py
@@ -1,14 +1,14 @@
-# Функція, яка буде рахувати тільки позитивні числа.
+# Функція, яка буде рахувати тільки негативні числа.
 def count():
-    if a <= 0 or b <= 0:
-        print('Enter only a positive numbers and not 0!')
+    if a >= 0 or b >= 0:
+        print('Enter only a negative numbers and not 0!')
         return 'Not correct value'
     else:
         return a + b
 
 
-a = 25
-b = 35
+a = -25
+b = -35
 
 # Викликаю функцію
 print(f'You entered: {a} and {b}')
