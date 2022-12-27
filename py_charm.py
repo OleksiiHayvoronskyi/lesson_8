1b7a051c34ed4a62713fccedc27aad4e166397bc
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2022-12-27 20:48:20 +0200

    Modified def count

--- py_charm.py
+++ py_charm.py
@@ -3,9 +3,18 @@
 # Вирішити конфлікти.
 
 
-# Функція, яка буде рахувати числа.
-def count(a, b):
-    return a + b
+# Функція, яка буде рахувати тільки позитивні числа.
+def count():
+    if a <= 0 or b <= 0:
+        print('Enter only a positive numbers and not 0!')
+        return 'Not correct value'
+    else:
+        return a + b
 
 
-print(count(15, 25))
+a = 25
+b = 35
+
+# Викликаю функцію
+print(f'You entered: {a} and {b}')
+print(f'{a} + {b} =', count())
